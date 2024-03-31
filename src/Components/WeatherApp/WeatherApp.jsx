ad0645ccb5d6722021ffb828d4efb8fcb6650a1e
EchoParratt <echoparratt@gmail.com>
2024-03-31 11:04:04 -0700

    Cleaned code up and added documentation

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react';  // Imports and State Initialization
 import './WeatherApp.css';
 
 import search_icon from '../Assets/search.png';
@@ -7,6 +7,8 @@ import cloud_icon from '../Assets/cloud.png';
 import rain_icon from '../Assets/rain.png';
 import snow_icon from '../Assets/snow.png';
 
+// We are using the OpenWeatherMap api
+// Thunderclient is used to check the specifics behind the calls
 const WeatherApp = () => {
   const api_key = '8f5af98cd2a407387922a2eaffa213d3';
   const [city, setCity] = useState('');
@@ -15,13 +17,14 @@ const WeatherApp = () => {
     date: '',
     description: '',
     temp: null,
-    icon: rain_icon
+    icon: null
   });
 
-  const [backgroundColor, setBackgroundColor] = useState(''); 
+const [backgroundColor, setBackgroundColor] = useState(''); 
 
-
-  const search = async () => {
+// Asynchronous function that is called when the user searches for a city's weather
+// Using a asynchronous function in this case is important since we are fetching data and we do not want to block other code in the process
+  const search = async () => {   
     if (city === '') {
       return;
     }
@@ -32,14 +35,24 @@ const WeatherApp = () => {
       if (!response.ok) {
         throw new Error('City not found');
       }
-      const data = await response.json();
 
-      const date = new Date().toLocaleDateString('en-US', {
+    // Converting the JSON-formatted response data into a JavaScript object
+    const data = await response.json();
+
+    // Caculating correct date according to the timezone of the city
+    const timezoneOffset = data.timezone * 1000;
+    const localTime = new Date(Date.now() + timezoneOffset);
+
+
+    const date = localTime.toLocaleDateString('en-US', {
         weekday: 'short',
         month: 'short',
-        day: 'numeric'
+        day: 'numeric',
+        timeZone: 'UTC' 
       });
 
+      // Used to determine which icon is to be displayed, perhaps changed if else statements to something more optimal like buckets
+      // Considering using switch statements here instead
       const getWeatherIcon = (iconCode) => {
         if (iconCode === "01d" || iconCode === "01n") {
           return clear_icon;
@@ -51,43 +64,46 @@ const WeatherApp = () => {
           return cloud_icon;
         } else if (iconCode === "09d" || iconCode === "09n") {
           return rain_icon;
-        } else if (iconCode === "010d" || iconCode === "010n") {
+        } else if (iconCode === "10d" || iconCode === "10n") {
           return rain_icon;
-        } else if (iconCode === "013d" || iconCode === "013n") {
+        } else if (iconCode === "13d" || iconCode === "13n") {
           return snow_icon;
         } else {
           return rain_icon;
         }
       };
 
+      // Background setter depending on the same variable as the above, "icon" in the api call
+      // Tried using switch statements 
       const getBackgroundColor = (iconCode) => {
         switch (iconCode) {
           case "01d":
           case "01n":
-            return '#FFD700'; // Gold for clear sky
+            return '#e27055'; // Clear
           case "02d":
           case "02n":
           case "03d":
           case "03n":
           case "04d":
           case "04n":
-            return '#B0C4DE'; // LightSteelBlue for clouds
+            return '#00cec9'; // Clouds
           case "09d":
           case "09n":
           case "10d":
           case "10n":
-            return '#4682B4'; // SteelBlue for rain
+            return '#0b84e3'; // rain
           case "13d":
           case "13n":
-            return '#F0F8FF'; // AliceBlue for snow
+            return '#2d3435'; // snow
           default:
-            return '#87CEEB'; // SkyBlue as default
+            return '#87CEEB'; // default, most likely won't be reached
         }
       };
 
       const bgColor = getBackgroundColor(data.weather[0].icon);
         setBackgroundColor(bgColor);
     
+      // Setting the variables on the weather-card
       const weatherIcon = getWeatherIcon(data.weather[0].icon);
       setWeather({
         city: data.name,
@@ -110,7 +126,7 @@ const WeatherApp = () => {
           className='cityInput'
           placeholder='Search'
           value={city}
-          onChange={(e) => setCity(e.target.value)}
+          onChange={(e) => setCity(e.target.value)}  // Code to used to add a enter button press search function (Found Online)
           onKeyPress={(e) => e.key === 'Enter' && search()}
         />
         <div className='search-icon' onClick={search}>
@@ -124,12 +140,13 @@ const WeatherApp = () => {
         <div className='weather-info'>
           <div className='inputed-city'>{weather.city}</div>
           <div className='date'>{weather.date}</div>
-          <div className='weather-type'>{weather.description}</div>
-          <div className='weather-temp'>{weather.temp ? `${weather.temp} °C` : ''}</div>
+          <div className='weather-type'>{weather.description}</div> 
+          <div className='weather-temp'>{weather.temp ? `${weather.temp} °C` : ''}</div> 
         </div>
       </div>
     </div>
   );
 };
+// The code within the weather-temp div is used to avoid showing "undefined °C" or "null °C" in the UI.
 
 export default WeatherApp;
